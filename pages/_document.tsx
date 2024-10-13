fdc4ad0e7b39808afb2bc99fabc15fa4acede6fd
ggjiny <jnss599@gmail.com>
2024-10-13 21:23:43 +0900

    Docs: opengraph setting

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,15 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { pretendard } from '@/lib/fonts';
+import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ko" className={pretendard.variable}>
+      <Head>
+        <meta name="description" content="내 취준 스타일로 알아보는 취준 꿀팁" />
+        <meta property="og:title" content="[Sulsul] 취준유형 테스트" />
+        <meta property="og:description" content="내 취준 스타일로 알아보는 취준 꿀팁" />
+        <meta property="og:image" content="/images/OG.png" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
