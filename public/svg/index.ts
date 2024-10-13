2001eb7b8f2c29f32d4fa9ff148f2e9a53fb72c5
ggjiny <jnss599@gmail.com>
2024-10-13 21:21:21 +0900

    Style: image, svg setting

---
+++ public/svg/index.ts
@@ -0,0 +1,18 @@
+export { default as BackgroundImg } from './bg.svg';
+
+export { default as BubbleImg } from './bubble.svg';
+export { default as ProfileImg } from './profile.svg';
+
+export { default as BulbIcon } from './icon-bulb.svg';
+export { default as GraphIcon } from './icon-graph.svg';
+
+export { default as LayerImg } from './layer.svg';
+export { default as SulsulImg } from './sulsul.svg';
+
+export { default as RedoIcon } from './icon-redo.svg';
+
+export { default as KakaoIcon } from './btn-kakao.svg';
+export { default as LinkIcon } from './btn-link.svg';
+
+export { default as TitleImg } from './title.svg';
+export { default as TitleImg2 } from './title2.svg';
