b69cf0171ecf5d4d44444be965b212cd88e6ac05
ggjiny <jnss599@gmail.com>
2024-10-14 22:01:56 +0900

    Style: change pretendard apply

--- pages/layout.tsx
+++ pages/layout.tsx
@@ -8,7 +8,9 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <Head>
         <title>[Sulsul] 취준유형 테스트</title>
       </Head>
-      <div className={`relative mx-auto max-w-[500px] ${pretendard} ${gmarketSans.variable}`}>
+      <div
+        className={`relative mx-auto max-w-[500px] ${pretendard.variable} ${gmarketSans.variable}`}
+      >
         {children}
       </div>
       <Script src="https://developers.kakao.com/sdk/js/kakao.js" strategy="afterInteractive" />
