276c574b0d1b5a37b8fa8f03e5f7a2679676e7db
ggjiny <jnss599@gmail.com>
2024-10-16 15:06:41 +0900

    Chore: add hotjar script

--- pages/layout.tsx
+++ pages/layout.tsx
@@ -14,6 +14,16 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         {children}
       </div>
       <Script src="https://developers.kakao.com/sdk/js/kakao.js" strategy="afterInteractive" />
+      <Script id="hotjar-script" strategy="afterInteractive">
+        {`(function(h,o,t,j,a,r){        
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};        
+        h._hjSettings={hjid:5173093,hjsv:6};        
+        a=o.getElementsByTagName('head')[0];        
+        r=o.createElement('script');r.async=1;        
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;        
+        a.appendChild(r);    
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
+      </Script>
     </>
   );
 }
